8d962d2c4b7eb30d8293d8d496a245b3af5768f7
taifmunsur <muta1001@gmail.com>
2016-12-14 12:57:14 -0600

    Update iot-project.ino

--- iot-project.ino
+++ iot-project.ino
@@ -4,7 +4,7 @@ void setup() {
 
 void loop() {
     if (digitalRead(D0) == HIGH) {
-        Particle.publish("Store Motion", "MOTION", 60, PRIVATE);
+        Particle.publish("Store Motion", "MOTION", 60, PRIVATE); // Publish to Particle Cloud when motion is detected
         while (digitalRead(D0) == HIGH); // hang tight here until motion stops
     }
 }
